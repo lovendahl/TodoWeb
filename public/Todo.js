9036248d452a4e2d47d20b0faf8fe1a93b519dbd
Lion laptop <lionvalleyco@gmail.com>
2025-01-04 16:09:12 +0100

    Test for Todo class

--- public/Todo.js
+++ public/Todo.js
@@ -3,4 +3,8 @@ class Todo {
         this.headline = headline;
         this.creationDate = Date.now();
     }
-}
+};
+module.exports = {Todo};
+
+//var t = new Todo("localHeadline");
+//console.log("todo: " + t);
--- public/Todo.test.js
+++ public/Todo.test.js
@@ -1,5 +1,18 @@
-const todo = require("./Todo.js");
+const t = require("./Todo.js");
 
-test('defaultTest',()=> {
-    expect(1).toBe(1);
+test('test todo class creatin',()=> {
+    var tmpTodo = new t.Todo("hl");
+    expect(tmpTodo.headline).toBe("hl");
+});
+
+test('test todo class with description',()=> {
+    var tmpTodo = new t.Todo("hl");
+        tmpTodo.description = "desc";
+    expect(tmpTodo.headline).toBe("hl");
+    expect(tmpTodo.description).toBe("desc");
+});
+
+test('test creation date',()=> {
+    var tmpTodo = new t.Todo("hl");
+    expect(tmpTodo.creationDate).toBeLessThan(Date.now()+1);
 });
