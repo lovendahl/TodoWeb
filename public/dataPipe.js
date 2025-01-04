d8a33e781bbaef3a9a60966ded72a7869c500a35
Lion laptop <lionvalleyco@gmail.com>
2025-01-04 11:51:41 +0100

    Post til /new

--- public/dataPipe.js
+++ public/dataPipe.js
@@ -1,4 +1,43 @@
-function updateNewTodo(){
+function updateNewTodo(headline,beskrivelse,clickEvent)
+    {
     console.log("New todo created... Sending request to backend.")
-    console.log("...not implemented yet");
-}
+    console.log("ClickEvent is: " + clickEvent)
+    postData();
+    async function postData(){
+        
+        clickEvent.preventDefault();
+        console.log("sending new todo");
+
+        const res = await fetch('http://localhost:8010/new',
+            {
+                method:'POST',
+                headers: {
+                    "Content-type":'application/json'
+                },
+                body: JSON.stringify({
+                    parcel: headline
+                })
+            })
+        };
+        postData();
+    }
+
+    /*
+    async function postData(){
+        
+        clickEvent.preventDefault();
+        console.log("sending new todo");
+
+        const res = await fetch('http://localhost:8010/new',
+            {
+                method:'POST',
+                headers: {
+                    "Content-type":'application/json'
+                },
+                body: JSON.stringify({
+                    parcel: headline
+                })
+            })
+        };
+        postData();
+    }*/
--- server.js
+++ server.js
@@ -8,3 +8,15 @@ app.listen(port,() =>{
     console.log('Server started. Listening on port ' + port);
 })
 
+app.post('/new',(req,res) =>
+    {
+        console.log("server got info at /new");
+        const {parcel} = req.body;
+        console.log("server: " + parcel);
+
+        if(!parcel) 
+            {
+                return res.status(400).send({status:'failed'});
+            }
+            res.status(200).send({status:'got new todo info'});
+    });
