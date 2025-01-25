50efe909ca09cef27049aee46d88616eab12d3c2
Lion laptop <lionvalleyco@gmail.com>
2025-01-25 08:38:59 +0100

    todo date and date string set

--- public/Todo.js
+++ public/Todo.js
@@ -1,10 +1,66 @@
 class Todo {
+  //TODO get set for description.
+  //TODO get set for status
+
+  #headline;
+  #creationDate;
+  #description;
+
   constructor(headline) {
     this.headline = headline;
-    this.creationDate = Date.now();
+    this.#creationDate = new Date();
+  }
+
+  get Headline() {
+    return this.#headline;
+  }
+
+  set Headline(headline) {
+    this.#headline = headline;
+  }
+  setDescription(description) {
+    this.#description = description;
+  }
+
+  getDescription() {
+    return this.#description;
+  }
+
+  get creationEpoc() {
+    return this.#creationDate.getTime();
+  }
+
+  getCreationDate_ToString() {
+    const options = {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    return this.#creationDate.toLocaleString("en-GB", options);
+  }
+  // return new Proxy(this, {
+  //   get: (_, prop) => this[prop],
+  //   set: (_, prop, value) => {
+  //     if (!(prop in this)) throw new Error('Prop does not exist!');
+  //     this[prop] = value;
+  //   }
+  // });
+}
+
+class Status {
+  static finished = new Status("finished");
+  static notStarted = new Status("notStarted");
+
+  constructor(name) {
+    this.name = name;
   }
 }
-module.exports = { Todo };
+module.exports = {
+  Todo,
+};
 
 //TODO Create a enmun with finished.
 //TODO Set new todo in controctor to noStarted
--- public/Todo.test.js
+++ public/Todo.test.js
@@ -1,18 +1,42 @@
 const t = require("./Todo.js");
 
-test('test todo class creatin',()=> {
-    var tmpTodo = new t.Todo("hl");
-    expect(tmpTodo.headline).toBe("hl");
+test("test todo class creatin", () => {
+  var tmpTodo = new t.Todo("hl");
+  expect(tmpTodo.headline).toBe("hl");
 });
 
-test('test todo class with description',()=> {
-    var tmpTodo = new t.Todo("hl");
-        tmpTodo.description = "desc";
-    expect(tmpTodo.headline).toBe("hl");
-    expect(tmpTodo.description).toBe("desc");
+test("test todo class with description", () => {
+  var tmpTodo = new t.Todo("hl");
+  tmpTodo.description = "desc"; //TODO det skal være en get /set i stedet.
+  expect(tmpTodo.headline).toBe("hl");
+  expect(tmpTodo.description).toBe("desc");
 });
 
-test('test creation date',()=> {
-    var tmpTodo = new t.Todo("hl");
-    expect(tmpTodo.creationDate).toBeLessThan(Date.now()+1);
+test("test creation date", () => {
+  var tmpTodo = new t.Todo("hl");
+  expect(tmpTodo.creationEpoc).toBeLessThan(Date.now() + 1);
+});
+
+test("getSetHeadline ", () => {
+  var tmp = new t.Todo("headline");
+  var headline = "myheadline";
+  tmp.Headline = headline;
+  expect(tmp.Headline).toBe(headline);
+});
+
+test("test print of creation date", () => {
+  var tmpTodo = new t.Todo("h2");
+  var checkDate = new Date();
+  const options = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  var d = new Date();
+  expect(tmpTodo.getCreationDate_ToString()).toBe(
+    checkDate.toLocaleString("en-GB", options)
+  );
 });
